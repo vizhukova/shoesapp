f33ffa35623bd8a290f282477c660179c6728a38
geferr <geferr@yandex.ru>
2016-04-12 11:45:34 +0300

    Add recover part to sign-in popover & sign-un

--- www/src/login/controller.js
+++ www/src/login/controller.js
@@ -1,9 +1,11 @@
 export default function($scope, $state, $ionicPopover) {
+
   $scope.animation = 'slide-in-up';
+  $scope.recovery = false;
 
   var popups = [
     {name: 'signinPopover', url: 'src/login/directives/signin.popover.html'},
-    {name: 'authPopover', url: 'src/shop/auth-popover.html'}
+    {name: 'signunPopover', url: 'src/login/directives/signun.popover.html'}
   ];
 
   popups.map((popup)=>{
@@ -17,10 +19,20 @@ export default function($scope, $state, $ionicPopover) {
     $scope[`open${popup.name}`] = ($event)=>{
       $scope[popup.name].show($event);
     };
-  })
+
+    $scope[`close${popup.name}`] = ($event)=>{
+      $scope.toggleRecovery();
+      $scope[popup.name].hide();
+    };
+  });
+
+  $scope.toggleRecovery = () => {
+    $scope.recovery = !$scope.recovery;
+  };
 
   $scope.toShop = () => {
     $scope.signinPopover.hide();
+    $scope.signunPopover.hide();
     $state.go('tab.shop')
   };
 }
