24393570f0233561bf2ecd18f0cbbc602b7e0ecd
Zhukova Veronika <veronikazhukova9@gmail.com>
2016-08-08 17:14:05 +0300

    1st commit

--- www/src/config.js
+++ www/src/config.js
@@ -1,7 +1,7 @@
 angular.module('starter.config', [])
 
 //.value('URL', 'http://localhost:8100/api/')
-.value('URL', 'http://shoes.mikero.ru/api/')
+.value('URL', 'https://shoes.mikero.ru/api/')
 
 
 
